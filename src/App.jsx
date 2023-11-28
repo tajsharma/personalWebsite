e1d5b1358f19873c737c7c186854a22d3e365516
tajsharma <sharma.tajinder01@gmail.com>
2023-11-27 17:03:31 -0800

    Add navbar and hero components to the home page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ const App =() =>{
   return (
     <BrowserRouter>
       <div className = "relative z-0 bg-primary">
-        
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar/>
+          <Hero/>
+        </div>
       </div>
     </BrowserRouter>
   )
