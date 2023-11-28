57288703909d168e5c3a145cbd717286516a4fab
tajsharma <sharma.tajinder01@gmail.com>
2023-11-27 22:06:18 -0800

    changed logo export to trace image, changed logo therefore

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import trace from "./trace.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -31,7 +31,7 @@ import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
 export {
-  logo,
+  trace,
   backend,
   creator,
   mobile,
