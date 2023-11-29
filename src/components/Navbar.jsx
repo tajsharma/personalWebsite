550d0049d3ed99f65959f7783ecb45d646200b89
tajsharma <sharma.tajinder01@gmail.com>
2023-11-28 16:55:40 -0800

    add hover proerty to navlinks and activate links if they are on that page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,12 @@ const Navbar = () => {
 
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link)=> (
-            <li>
+            <li 
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(nav.title)}>
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
