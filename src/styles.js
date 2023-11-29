7f8e817eba66bf48a721312be5a95d1a72befee0
tajsharma <sharma.tajinder01@gmail.com>
2023-11-28 17:26:48 -0800

    add styles for left and right padding excusively

--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,7 @@
 const styles = {
     paddingX: "sm:px-16 px-6",
+    paddingRight: "sm:pr-16 pr-6",
+    paddingLeft: "sm:pl-8 pl-6",
     paddingY: "sm:py-16 py-6",
     padding: "sm:px-16 px-6 sm:py-16 py-10",
   
