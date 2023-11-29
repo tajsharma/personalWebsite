008999ab478191fd297848e7dcf8f7def4e6a893
tajsharma <sharma.tajinder01@gmail.com>
2023-11-28 16:32:03 -0800

    Map navlinks to a list in the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,21 @@ const Navbar = () => {
           }
         >
           <img src = {logo} alt = "logo" className="w-9 h-9 object-contain"/>
+
+          <p className='text-white text-[18px] font-bold cursor-pointer'>
+            Taj Sharma<span className=''> | Developer Portfolio</span>
+          </p>
         </Link>
+
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link)=> (
+            <li>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+
+        </ul>
+
       </div>
 
     </nav>
