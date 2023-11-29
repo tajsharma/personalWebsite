27d3f4c4db258556c6f22d268d583ded2f359f44
tajsharma <sharma.tajinder01@gmail.com>
2023-11-28 21:07:23 -0800

    add a menu dropdown for smaller sized devices, as well as a closed toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,8 @@ import { navLinks } from '../constants';
 import { menu, close, logo} from '../assets';
 
 const Navbar = () => {
-  const [active, setActive] = useState("")
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className = {`${styles.paddingLeft} ${styles.paddingRight} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
 
@@ -20,9 +21,9 @@ const Navbar = () => {
           }
           }
         >
-          <img src = {logo} alt = "logo" className="w-9 h-9 object-contain"/>
+          <img src = {logo} alt = "logo" className="w-10 h-11 object-contain"/>
 
-          <p className='text-white text-[18px] font-bold cursor-pointer'>
+          <p className='italic text-white text-[18px] font-bold cursor-pointer'>
             Taj Sharma<span className=''> | Developer Portfolio</span>
           </p>
         </Link>
@@ -33,13 +34,22 @@ const Navbar = () => {
               key={link.id}
               className={`${
                 active === link.title ? "text-white" : "text-secondary"
-              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              } italic hover:text-white text-[18px] font-medium cursor-pointer`}
               onClick={() => setActive(link.title)}>
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
-
         </ul>
+
+        <div className='sm:hidden flex flex-1 justify-end items center'>
+          <img 
+            src={toggle ? close : menu}
+            alt='menu' 
+            className='w-[28px] h-[28[x] object-contain cursor-pointer'
+            onClick={()=>setToggle(!toggle)}
+          />
+        </div>
+
 
       </div>
 
