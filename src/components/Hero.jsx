12f5c41672a868f31b1fe50e264e44b143a58947
tajsharma <sharma.tajinder01@gmail.com>
2023-12-02 20:09:00 -0800

    change pill from circle to a square shape by changing border tailwind property

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#000000]'/>
+          <div className='w-5 h-5 rounded-none bg-[#000000]'/>
           <div className='w-1 sm:h-80 h-40 alt-black-gradient'/>
         </div>
 
@@ -23,7 +23,7 @@ const Hero = () => {
         </div>
 
       </div>
-      
+
       <ComputersCanvas/>
     </section>
   )
