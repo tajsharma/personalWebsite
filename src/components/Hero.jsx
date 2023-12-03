a670885fb57b06359baa5d360b7373f14d8ce3b7
tajsharma <sharma.tajinder01@gmail.com>
2023-12-02 18:17:46 -0800

    Add the the computer canvas outside of other divs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,9 +19,12 @@ const Hero = () => {
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I love to build pretty websites, algorithms, <br className='sm:block hidden' />
             web applications and pretty much anything 3D
-          </p>
+          </p>    
         </div>
+
       </div>
+      
+      <ComputersCanvas/>
     </section>
   )
 }
