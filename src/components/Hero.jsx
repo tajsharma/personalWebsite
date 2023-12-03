9ad08e72e8516ece8d45cdfb622012397f85cd00
tajsharma <sharma.tajinder01@gmail.com>
2023-12-02 18:01:40 -0800

    Apply the alt black to the line under the dot:

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,18 @@ import { ComputersCanvas } from './canvas'
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
-      
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7x1 mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#000000]'/>
+          <div className='w-1 sm:h-80 h-40 alt-black-gradient'/>
+        </div>
+
+        <div>
+          <h1 className='font-bold text-black'>
+            Hi, my name is <span className='text-[#ff7ae1] font-bold'> Taj!</span>
+          </h1>
+        </div>
+      </div>
     </section>
   )
 }
