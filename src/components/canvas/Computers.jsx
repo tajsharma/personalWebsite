6f8d4347db6dd52907210900992ca1161ae67fa7
tajsharma <sharma.tajinder01@gmail.com>
2024-01-03 16:19:50 -0800

    zoom out of the scene by 2

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -58,7 +58,7 @@ const ComputersCanvas = () => {
       frameloop='demand'
       shadows
       dpr={[1, 2]}
-      camera={{ position: [20, 1, 5], fov: 25 }}
+      camera={{ position: [22, 1, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
