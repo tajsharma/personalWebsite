1415e4ed0682f287219c5374ea19bb09273247a2
tajsharma <sharma.tajinder01@gmail.com>
2024-01-03 16:25:12 -0800

    Make ball inside loop -> black

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
 
       <ComputersCanvas/>
 
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center ">
         <a href="#about">
           <div className = "w-[35px] h-[63px] rounded-3xl border-4 border-secondary flex justify-center  items-start p-2">
           <motion.div
@@ -38,7 +38,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
+              className='w-3 h-3 rounded-full bg-black mb-1'
             />
           </div>
         </a>
