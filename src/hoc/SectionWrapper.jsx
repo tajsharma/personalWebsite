592b3136bba4b47834242727abed24531d58f647
tajsharma <sharma.tajinder01@gmail.com>
2024-01-06 00:12:23 -0800

    return a motion.section containing our component

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import {motion} from 'framer-motion';
+import {styles} from '../styles';
+import { staggerContainer } from '../utils/motion';
 
-const SectionWrapper = () => {
-    return(
-        <div>SectionWrapper</div>
-    )
+
+const SectionWrapper = (Component, idName) => {
+    function HOC(){
+        return(
+            <motion.section>
+                <Component/>
+            </motion.section>
+        )
+    }
 }
 
 export default SectionWrapper
