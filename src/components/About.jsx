56794c7b585e4e74d6df52e879aa4bc61cc2ee3b
tajsharma <sharma.tajinder01@gmail.com>
2024-01-05 23:58:53 -0800

    Change the font and padding of the cards of services

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,26 @@ import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({index, title, icon}) => {
   return (
-    <p>{title}</p>
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants= {fadeIn("right", "spring" , 0.5*index, .75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+        >
+          <div
+          options={{
+            max:45,
+            scale: 1,
+            speed: 450
+          }}
+          className="bg-tertiary rounded-[20px] py-5 min-h-[280px] flex justify-evenly
+                     items-center flex-col"
+          >
+            <img src={icon} alt={title} className="w-15 h-15 object-contain"/>
+            <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+
+          </div>
+      </motion.div>
+    </Tilt>
   )
 }
 const About = () => {
