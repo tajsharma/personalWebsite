c26c92f08d9a239764a50f512b75351c776b50b3
tajsharma <sharma.tajinder01@gmail.com>
2024-01-06 02:05:03 -0800

    added stagger to the import alonig with motion

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,4 +1,4 @@
-import {motion} from 'framer-motion';
+import {motion, stagger} from 'framer-motion';
 import {styles} from '../styles';
 import { staggerContainer } from '../utils/motion';
 
@@ -6,7 +6,14 @@ import { staggerContainer } from '../utils/motion';
 const SectionWrapper = (Component, idName) => 
     function HOC(){
         return(
-            <motion.section>
+            <motion.section
+                variants={staggerContainer()}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{once: true, amount: 0.25}}
+                className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+            >
+                <span className='hash-span' id={idName}>&nbsp;</span>
                 <Component/>
             </motion.section>
         );
