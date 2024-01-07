4ed29f85e529eaf68decb56df9161e65f18bddcb
tajsharma <sharma.tajinder01@gmail.com>
2024-01-06 19:47:00 -0800

    Add flatshading to the decal

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -14,7 +14,7 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float>
+    <Float speed={1.5} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
@@ -29,6 +29,7 @@ const Ball = (props) => {
         <Decal
           position={[0,0,1]}
           rotation={[2* Math.PI, 0, 6.25]}
+          flatShading
           map={decal}
         />
       </mesh>
