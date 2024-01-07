469ec6c51549fe5230e809d41ac32d6d8d2ae2a9
tajsharma <sharma.tajinder01@gmail.com>
2024-01-06 18:45:11 -0800

    copy over the canvas from the other 3d object

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
 
-const Ball = () => {
+import CanvasLoader from "../Loader";
+
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
+
   return (
     <div>Ball</div>
+  )
+}
+
+const BallCanvas = ({icon}) => {
+  return(
+    <Canvas
+      frameloop='demand'
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [22, 1, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
   )
 }
 
