0da5d732bd01c78d76d4cda0dbc38ce870e8b708
tajsharma <sharma.tajinder01@gmail.com>
2024-01-06 19:51:51 -0800

    apply the section wrapper to the perosnal projects div

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,23 @@
-import Tilt from 'react-tilt'
+import React from "react";
+import {Tilt} from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <h2 className = { styles.sectionHeadText}>Personal Projects</h2>
+      </motion.div>
+
+    </>
   )
 }
 
-export default Works
+
+export default SectionWrapper(Works, "");
