df8880ebea70a8f85fa70c1280bb572e20a14449
tajsharma <sharma.tajinder01@gmail.com>
2024-01-06 19:19:41 -0800

    change the color of the mesh around the poly balls

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -20,7 +20,7 @@ const Ball = (props) => {
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1,1]}/>
         <meshStandardMaterial 
-          color='#ffffeb'
+          color='#ff98eb'
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
