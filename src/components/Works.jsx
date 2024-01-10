1b6bfbf53041093629cff56532ab4fd030874309
Taj Sharma <sharma.tajinder01@gmail.com>
2024-01-09 16:40:21 -0800

    Add stylng to base card holder for projects

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,6 +8,19 @@ import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+const ProjectCard = ({
+  index,name,description, tags,image,source_code_link,}) => {
+    return (
+      <motion.div variants={fadeIn("up","spring", index*0.5,0.75)}>
+        <Tilt
+          options={{max:45,scale:1,speed:450}}
+          className = "bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+        >    
+        </Tilt>
+      </motion.div>
+    )
+}
+
 const Works = () => {
   return (
     <>
