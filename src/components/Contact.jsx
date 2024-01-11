3610d536fc344f54655f11a5e1eb8f00495bcd78
Taj Sharma <sharma.tajinder01@gmail.com>
2024-01-11 01:56:55 -0800

    Change the reply message

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -50,7 +50,7 @@ const Contact = () => {
       .then(
         () => {
           setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible.");
+          alert("Thank you for your message! I will get back to you as soon as possible.");
 
           setForm({
             name: "",
