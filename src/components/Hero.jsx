79149eff69f5cddb40e67d11e095cabada9e8325
Taj Sharma <sharma.tajinder01@gmail.com>
2024-01-11 14:30:13 -0800

    Change the border color of the  scroll  button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center ">
         <a href="#about">
-          <div className = "w-[35px] h-[63px] rounded-3xl border-black border-4 border-secondary flex justify-center  items-start p-2">
+          <div className = "w-[35px] h-[63px] rounded-3xl border-black border-4 flex justify-center  items-start p-2">
           <motion.div
               animate={{
                 y: [0, 24, 0],
