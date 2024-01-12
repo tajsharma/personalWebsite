26334def4337599f2ca37db0315bb101b50b882c
Taj Sharma <sharma.tajinder01@gmail.com>
2024-01-11 16:34:27 -0800

    change the text color to white

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -18,7 +18,7 @@ const ProjectCard = ({
             scale: 1,
             speed: 450,
           }}
-          className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+          className='bg-[#776EFF] p-5 rounded-2xl sm:w-[360px] w-full'
         >
           <div className='relative w-full h-[230px]'>
             <img
@@ -43,7 +43,7 @@ const ProjectCard = ({
   
           <div className='mt-5'>
             <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-            <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+            <p className='mt-2 text-white text-[14px]'>{description}</p>
           </div>
   
           <div className='mt-4 flex flex-wrap gap-2'>
