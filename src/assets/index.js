e7c426aca87018505ad21d6b8c1c4e9921c84c2c
Taj Sharma <sharma.tajinder01@gmail.com>
2024-02-03 18:10:53 -0800

    import new asset

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import logo from "./new-logo.png";
+import linkedin_logo from "./linkedin-icon.png"
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -37,6 +38,7 @@ import tripguide from "./tripguide.png";
 
 export {
   logo,
+  linkedin_logo,
   backend,
   creator,
   mobile,
